10234ac9b625690f304c5250191ea29a8f5e1a89
Destan <alessandroporcheddu000@gmail.com>
2024-11-22 19:44:36 +0100

    93: implemented controller class

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,43 @@
 package it.unibo.mvc;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    List<String> history;
+    String nexString; 
+
+    @Override
+    public void setNextString(String string) {
+        Objects.requireNonNull(string);
+        this.nexString = string;
+    }
+
+    @Override
+    public String getNextString() {
+        if (nexString == null) {
+            throw new IllegalStateException("Current string unset!");
+        }
+        return this.nexString;
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return this.history;
+    }
+
+    @Override
+    public void printCurrentString() {
+        if (nexString == null) {
+            throw new IllegalStateException("Current string unset!");
+        }
+        System.out.println(nexString);
+        history.add(nexString);
+    }
 
 }
